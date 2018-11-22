8b27670c93d7563ee45a3c42743a0552e18cdad7
veia-mac <917971502@qq.com>
2018-11-22 12:28:41 +0800

    java-producer-consumer

---
+++ java-producer-consumer/src/main/java/veiasai/Con.java
@@ -0,0 +1,37 @@
+package veiasai;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class Con
+{
+    public static void main( String[] args )
+    {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "212.64.16.70:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        TopicPartition p = new TopicPartition("test",0);
+        consumer.assign(Arrays.asList(p));
+        consumer.seekToBeginning(Arrays.asList(p));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+    }
+}
---
+++ java-producer-consumer/src/main/java/veiasai/FileConfig.java
@@ -0,0 +1,4 @@
+package veiasai;
+
+public class FileConfig {
+}
